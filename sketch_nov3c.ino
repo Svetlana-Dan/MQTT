f9c6a9c449fcf332132ab63f5bbef3f76f05ec2d
Svetlana-Dan <76607753+Svetlana-Dan@users.noreply.github.com>
2022-11-24 22:21:45 +0800

    Update sketch_nov3c.ino

--- sketch_nov3c.ino
+++ sketch_nov3c.ino
@@ -1,20 +1,25 @@
 #define led_pin 6
-int s;
 
 void setup() {
   Serial.begin(9600);
+  while (!Serial){
+    
+  }
   pinMode(led_pin, OUTPUT);
 }
 
 void loop() {
-  while (Serial.available()){
-    s = Serial.read();
-  }
-  if( s == '1'){
-    digitalWrite(led_pin, HIGH);
-  }
-  else if(s == '0'){
-    digitalWrite(led_pin, LOW);
+  while (Serial.available() > 0){
+    char message = Serial.read();
+    Serial.println(message);
+    switch(message) {
+      case '1':
+        digitalWrite(led_pin, HIGH);
+      break;
+      case '0':
+        digitalWrite(led_pin, LOW);
+      break;
+    }
   }
 }
 
