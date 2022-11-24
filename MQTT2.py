ada3a520eb841b675454a38f527beff2ea93fcac
Svetlana-Dan <76607753+Svetlana-Dan@users.noreply.github.com>
2022-11-24 22:28:34 +0800

    Update MQTT2.py

--- MQTT2.py
+++ MQTT2.py
@@ -3,105 +3,75 @@ import random
 import time
 import serial
 
-UNIQUE_ID = 999
-port = "COM9"
-ser = serial.Serial(port, 9600)
-
-min_ =0
-min_illum = 0
-max_illum = 0
-stack = []
-stack_len = 5
-
-### функция включения светодиода при проходе порога
-def illum(data, topic):
-    global max_illum
-    global min_illum
-    if("/max" in topic):
-        max_illum = data
-    elif("/min" in topic):
-        min_illum = data
-    else:
-        mean = (max_illum+min_illum)/2
-        if (data>=mean):
-            ser.write("1".encode())
-        else:
-            ser.write("0".encode())
-        print("mean =", mean, "data =", data)
-        
-
-
-### Функция, включущая светодиод при уменьшии значей датчика
-def dec_inc(data):
-    print(f"recieved sensor level{ data}")
-    global stack
-    if(len(stack)>1):
-        dw_cnt = 0
-        up_cnt = 0
-        for i in range(1, len(stack)):
-            if (stack[i] < stack[i-1]):
-                dw_cnt += 1
-        for i in range(1, len(stack)):
-            if (stack[i] >= stack[i-1]):
-                up_cnt += 1
-        if(dw_cnt == len(stack)-1):
-            ser.write("1".encode())
-        elif(up_cnt == len(stack)-1):
-            ser.write("0".encode())
-
-### тестовая функция для записи на ардуино
-def write(data):       
-    print(f"recieved command {data}")
-    if (data == "1"):
-        ser.write("1".encode())
-        time.sleep(2)
-    if (data =="0"):
-        ser.write("0".encode())
-        time.sleep(2)
-
-def on_message(client, data, message):
-    data = float(message.payload.decode("utf-8"))
-    topic = message.topic
-    global stack
-    global stack_len
-    # topic = str(message.topic.decode("utf-8"))
-    print(f"Received message: {data}")
-    if ("/stream" in topic):
-        stack.append(data)
-        if len(stack) > stack_len:
-            stack.pop(0)
-    # dec_inc(data)  ### раскоментировать для проверки 4 пункта задания
-    
-    illum(data, topic)
-    return data
-    
-
-
 broker = "broker.emqx.io" 
 
 client = mqtt_client.Client(f"lab_{random.randint(10000, 99999)}")
-client.on_message = on_message
 
+def get_connection(port):
+    ser = serial.Serial(port, timeout=1)
+    return ser
 
-try:
+arr = []
+avg_all = 0
+avg = 0
+duration = 20
+stinput = True
+
+if __name__ == '__main__':
+    ser = get_connection("COM3")
     client.connect(broker)
-except Exception:
-    print("Failed to connect. Check network")
-    exit()    
+    print("""Выберете команду:
+            1 - передача моментальных значений сеносора
+            2 - передача усредненных данных
+            3 - потоковая передача данных
+            4 - Stream min, max and current values (20 seconds)
+            5 - по порогу включение света""")
 
-client.loop_start()
+    while(True):
+        if stinput:
+            command = int(input("Введите: "))
+
+        while ser.inWaiting() < 2:
+            pass
+        
+        val = ser.read(2)
+        result = val.decode()
+
+        if(result!=""):
+            result = int(result)
+            arr = [result] + arr
+            avg_all += int(result)
+        if(len(arr)>=100):
+            avg = avg_all / 100
+            avg_all -= arr.pop()
+        if(len(arr) < 100 and len(arr) > 0):
+            avg = avg_all / len(arr)
 
 
-print("Subscribing")    
-
-client.subscribe(f'lab/{UNIQUE_ID}/photo/instant')
-client.subscribe(f'lab/{UNIQUE_ID}/photo/average')
-    
-client.subscribe(f'lab/{UNIQUE_ID}/photo/min')
-client.subscribe(f'lab/{UNIQUE_ID}/photo/max')
-client.subscribe(f'lab/{UNIQUE_ID}/photo/stream')
-client.subscribe(f'lab/{UNIQUE_ID}/photo/porog')
-time.sleep(600)
+        if command == 1:
+            client.publish('lab/UNIQUE_ID/photo/instant', result)
+        elif command == 2:
+            client.publish('lab/UNIQUE_ID/photo/averge', avg)
+        elif command == 3:
+            if stinput:
+                timer_start = time.time()
+                stinput = False
+            if time.time() - timer_start >= duration:
+                stinput = True
+            client.publish('lab/UNIQUE_ID/photo/stream', result)
+        elif command == 4:
+            if stinput:
+                timer_start = time.time()
+                stinput = False
+            if time.time() - timer_start >= duration:
+                stinput = True
+            client.publish('lab/UNIQUE_ID/photo/stream4', result)
+        elif command == 5:
+            if stinput:
+                timer_start = time.time()
+                stinput = False
+            if time.time() - timer_start >= duration:
+                stinput = True
+            client.publish('lab/UNIQUE_ID/photo/mean', (max(arr) + min(arr)) / 2)
+            client.publish('lab/UNIQUE_ID/photo/porog', result)
 client.disconnect()
-client.loop_stop()
-print("Stop communication")
