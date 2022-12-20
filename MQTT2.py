b19ceea3c5ddfa467c871bfd2e1ba0eb9e3cbac7
Svetlana-Dan <76607753+Svetlana-Dan@users.noreply.github.com>
2022-12-20 16:31:48 +0800

    Update MQTT2.py

--- MQTT2.py
+++ MQTT2.py
@@ -7,7 +7,7 @@ broker = "broker.emqx.io"
 
 client = mqtt_client.Client(f"lab_{random.randint(10000, 99999)}")
 
-def get_connection(port):
+def get_connection(port): //подключение к порту
     ser = serial.Serial(port, timeout=1)
     return ser
 
@@ -17,7 +17,7 @@ avg = 0
 duration = 20
 stinput = True
 
-if __name__ == '__main__':
+if __name__ == '__main__': //подключаемся и выводим сообщение
     ser = get_connection("COM3")
     client.connect(broker)
     print("""Выберете команду:
@@ -27,34 +27,34 @@ if __name__ == '__main__':
             4 - Stream min, max and current values (20 seconds)
             5 - по порогу включение света""")
 
-    while(True):
+    while(True): //чтобы можно было ввести команду
         if stinput:
-            command = int(input("Введите: "))
+            command = int(input("Введите: ")) //считываем номер команды
 
         while ser.inWaiting() < 2:
             pass
         
-        val = ser.read(2)
-        result = val.decode()
+        val = ser.read(2) //считываю значения
+        result = val.decode() //декодим
 
-        if(result!=""):
-            result = int(result)
-            arr = [result] + arr
-            avg_all += int(result)
-        if(len(arr)>=100):
-            avg = avg_all / 100
-            avg_all -= arr.pop()
-        if(len(arr) < 100 and len(arr) > 0):
-            avg = avg_all / len(arr)
+        if(result!=""): //если не пустая строка 
+            result = int(result) //переводим в инт
+            arr = [result] + arr //добавляем в массив
+            avg_all += int(result) //суммируем все что приходит
+        if(len(arr)>=100): //если длина массива больше 100
+            avg = avg_all / 100 //чтобы найти среднее
+            avg_all -= arr.pop() //удаляем самый старый
+        if(len(arr) < 100 and len(arr) > 0): //если еще не 100
+            avg = avg_all / len(arr) //находим среднее
 
 
-        if command == 1:
-            client.publish('lab/UNIQUE_ID/photo/instant', result)
+        if command == 1: 
+            client.publish('lab/UNIQUE_ID/photo/instant', result) //моментальные значения в диапазоне от 0 до 100
         elif command == 2:
-            client.publish('lab/UNIQUE_ID/photo/averge', avg)
+            client.publish('lab/UNIQUE_ID/photo/averge', avg) //передаем средние данные
         elif command == 3:
             if stinput:
-                timer_start = time.time()
+                timer_start = time.time() //
                 stinput = False
             if time.time() - timer_start >= duration:
                 stinput = True
