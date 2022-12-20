174c59cc4cce944c6757a956238f3755b6fc3830
Svetlana-Dan <76607753+Svetlana-Dan@users.noreply.github.com>
2022-12-20 16:46:33 +0800

    Update sketch_nov3b.ino

--- sketch_nov3b.ino
+++ sketch_nov3b.ino
@@ -4,13 +4,13 @@ int count=0;
 void setup()
 {
   Serial.begin(9600);
-  pinMode(led_pin, INPUT);
+  pinMode(led_pin, INPUT); 
 }
 
 void loop()
 {
-  int val = analogRead(led_pin);
-  val = map(val, 0, 1023, 0, 100);
+  int val = analogRead(led_pin); //записываем
+  val = map(val, 0, 1023, 0, 100); // значение с фоторезистора и конв от 0 до 100
   delay(100);
   Serial.print(val);
 }
